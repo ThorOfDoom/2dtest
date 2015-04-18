b8da41d4537e2486740ed2d0b28c511b9224500e
Thor-Bjørn Böhme <thor.of.doom@gmail.com>
2015-04-18 07:20:03 +0200

    adds basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(PlayerInputController))]
+public class Player : MonoBehaviour
+{
+	public float walkVelocity;
+
+	PlayerInputController playerInputController;
+	Rigidbody2D body;
+
+	private bool shouldMove = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerInputController = GetComponent<PlayerInputController> ();
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		CheckInputs ();
+		MovePlayer ();
+	}
+
+	void CheckInputs ()
+	{
+		//check if we should move (left/right)
+		if (playerInputController.moving != 0) {
+			shouldMove = true;
+		} else {
+			shouldMove = false;
+		}
+	}
+
+	void MovePlayer ()
+	{
+		if (shouldMove) {
+			body.velocity = new Vector2 (walkVelocity * playerInputController.moving, body.velocity.y);
+			transform.localScale = new Vector3 (transform.localScale.x * (body.velocity.x > 0 ? 1 : -1), transform.localScale.y * 1, transform.localScale.z * 1);
+		} else {
+			body.velocity = new Vector2 (0.0f, body.velocity.y);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerInputController : MonoBehaviour
+{
+	public int moving;
+	private Dictionary<string, string> keyBindings;
+	// Use this for initialization
+	void Start ()
+	{
+		keyBindings = GetKeyBindings ();
+	}
+
+	void Update ()
+	{
+		moving = 0;
+		if (Input.GetKey (keyBindings ["left"]) || Input.GetKey (keyBindings ["leftAlt"])) {
+			moving -= 1;
+		}
+		if (Input.GetKey (keyBindings ["right"]) || Input.GetKey (keyBindings ["rightAlt"])) {
+			moving += 1;
+		}
+	}
+
+	/*
+	 * reads KeyBindings.txt and parses the values into the keyBindings Dictionary.
+	 */
+	Dictionary<string, string> GetKeyBindings ()
+	{
+		Dictionary<string, string> keys = new Dictionary<string, string> ();
+		TextAsset textFile = (TextAsset)Resources.Load ("KeyBindings", typeof(TextAsset));
+		System.IO.StringReader textStream = new System.IO.StringReader (textFile.text);
+		string line;
+
+		while ((line = textStream.ReadLine()) != null) {
+			if (!string.IsNullOrEmpty (line)) {
+				if (!line.StartsWith ("#")) {
+					string[] binding = line.Split ('=');
+
+					//if only one key set add the second
+					if (binding.Length == 2) {
+						binding [2] = binding [1];
+					}
+					
+					keys.Add (binding [0], binding [1]);
+					keys.Add (binding [0] + "Alt", binding [2]);
+				}
+			}
+		}
+		return keys;
+	}
+}

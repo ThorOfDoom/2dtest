70ae59fabe5d4bdc62fcb1daa9505cf2a1a06303
Thor-Bjørn Böhme <thor.of.doom@gmail.com>
2015-04-28 18:55:46 +0200

    updates the movement II

---
+++ Assets/Scripts/DebugLine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugLine : MonoBehaviour
+{
+	public float lineDuration;
+
+	private Vector2 oldPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		oldPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.DrawLine (oldPos, transform.position, Color.green, lineDuration);
+		oldPos = transform.position;
+	}
+}

e94015c556ba468db919a31c9dc33164d9ba7358
StefanOrt <stefan@ort.dk>
2015-10-08 21:24:55 +0200

    adds the beginning of the fancy background

--- Assets/Scripts/Data/The.cs
+++ Assets/Scripts/Data/The.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 public class The : MonoBehaviour
@@ -11,6 +12,7 @@ public class The : MonoBehaviour
 	public int MediumModuleCount;
 	public int HardModuleCount;
 	public int TextureCount;
+	public Dictionary<string, int[]> BackgroundTextureCounts;
 
 	void Awake ()
 	{
@@ -19,7 +21,7 @@ public class The : MonoBehaviour
 			data = this;
 		} else if (data != this) {
 			Destroy (gameObject);
-		}
+		}	
 	}
 
 	void Start ()
@@ -33,6 +35,7 @@ public class The : MonoBehaviour
 		GetMediumModuleCount ();
 		GetHardModuleCount ();
 		GetTextureCount ();
+		GetBackgroundTextureCounts ();
 	}
 
 	void GetEasyModuleCount ()
@@ -57,5 +60,26 @@ public class The : MonoBehaviour
 	{// textures are allways mad ein sets so we only need to check one directory
 		var textures = Resources.LoadAll ("Textures/Easy", typeof(Material));
 		TextureCount = textures.Length;
+	}
+
+	void GetBackgroundTextureCounts ()
+	{
+		BackgroundTextureCounts = new Dictionary<string, int[]> ();
+
+		var tier1 = Resources.LoadAll ("Backgrounds/Easy/1", typeof(TextAsset));
+		var tier2 = Resources.LoadAll ("Backgrounds/Easy/2", typeof(TextAsset));
+		var tier3 = Resources.LoadAll ("Backgrounds/Easy/3", typeof(TextAsset));
+		BackgroundTextureCounts.Add ("Easy", new int[3]{tier1.Length, tier2.Length, tier3.Length});
+		/*
+		tier1 = Resources.LoadAll ("Backgrounds/Medium/1", typeof(TextAsset));
+		tier2 = Resources.LoadAll ("Backgrounds/Medium/2", typeof(TextAsset));
+		tier3 = Resources.LoadAll ("Backgrounds/Medium/3", typeof(TextAsset));
+		BackgroundTextureCounts.Add ("Medium", new int[3]{tier1.Length, tier2.Length, tier3.Length});
+		
+		tier1 = Resources.LoadAll ("Backgrounds/Hard/1", typeof(TextAsset));
+		tier2 = Resources.LoadAll ("Backgrounds/Hard/2", typeof(TextAsset));
+		tier3 = Resources.LoadAll ("Backgrounds/Hard/3", typeof(TextAsset));
+		BackgroundTextureCounts.Add ("Hard", new int[3]{tier1.Length, tier2.Length, tier3.Length});
+		*/
 	}
 }
